b4a6ecf083440e7fa9bdede41a1803faf83f0d71
Ambar Sood <104883218+ambarsood@users.noreply.github.com>
2024-06-11 23:16:16 +0530

    Time: 46 ms (15.47%) | Memory: 16.5 MB (94.53%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,24 +3,18 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i,j,k=0,0,0
-        temp=nums1.copy()
-        while i<m and j<n:
-            if temp[i]<nums2[j]:
-                nums1[k]=temp[i]
-                i+=1
-                k+=1
+        if n == 0 :return
+        len1 = len(nums1)
+        end_idx = len1-1
+        while n > 0 and m > 0 :
+            if nums2[n-1] >= nums1[m-1]:
+                nums1[end_idx] = nums2[n-1]
+                n-=1
             else:
-                nums1[k]=nums2[j]
-                j+=1
-                k+=1
-        while i<m:
-            nums1[k]=temp[i]
-            i+=1
-            k+=1
-        while j<n:
-            nums1[k]=nums2[j]
-            j+=1
-            k+=1
-
-      
+                nums1[end_idx] = nums1[m-1]
+                m-=1
+            end_idx-=1
+        while n > 0:
+            nums1[end_idx] = nums2[n-1]
+            n-=1
+            end_idx-=1
