a0168233cf5cb3ae7f1ebfe3a808a93211781e1f
Ambar Sood <104883218+ambarsood@users.noreply.github.com>
2024-06-11 23:06:10 +0530

    Time: 42 ms (40.50%) | Memory: 16.7 MB (6.06%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,7 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        del nums1[m:]
-        nums1.extend(nums2)
-        nums1.sort()
-     
+        i,j,k=0,0,0
+        temp=nums1.copy()
+        while i<m and j<n:
+            if temp[i]<nums2[j]:
+                nums1[k]=temp[i]
+                i+=1
+            else:
+                nums1[k]=nums2[j]
+                j+=1
+            k+=1
+        while i<m:
+            nums1[k]=temp[i]
+            i+=1
+            k+=1
+        while j<n:
+            nums1[k]=nums2[j]
+            j+=1
+            k+=1
+
+      
