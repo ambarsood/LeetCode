8d29fea2b0fd1065b7fa0dea03c812cb293f4a69
Ambar Sood <104883218+ambarsood@users.noreply.github.com>
2024-06-11 23:06:45 +0530

    Time: 41 ms (47.61%) | Memory: 16.7 MB (6.06%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -9,10 +9,11 @@ class Solution:
             if temp[i]<nums2[j]:
                 nums1[k]=temp[i]
                 i+=1
+                k+=1
             else:
                 nums1[k]=nums2[j]
                 j+=1
-            k+=1
+                k+=1
         while i<m:
             nums1[k]=temp[i]
             i+=1
